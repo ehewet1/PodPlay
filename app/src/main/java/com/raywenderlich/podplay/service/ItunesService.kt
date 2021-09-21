94b4d46f047b037b009b15a5a4c1f13c5a9e3fe3
ehewet1 <ehewett32425@live.com>
2021-09-20 19:46:15 -0500

    1st Commit - Ch 20 and Ch 21

---
+++ app/src/main/java/com/raywenderlich/podplay/service/ItunesService.kt
@@ -0,0 +1,26 @@
+package com.raywenderlich.podplay.service
+
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.http.GET
+import retrofit2.http.Query
+import retrofit2.converter.gson.GsonConverterFactory
+
+
+interface ItunesService {
+
+    @GET("/search?media=podcast")
+
+    suspend fun searchPodcastByTerm(@Query("term") term: String): Response<PodcastResponse>
+
+    companion object{
+
+        val instance: ItunesService by lazy {
+            val retrofit = Retrofit.Builder()
+                .baseUrl("https://itunes.apple.com")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+            retrofit.create(ItunesService::class.java)
+        }
+    }
+}
