8900dfa003c2d04a5d0623f67b7b8f1d9b42bbe9
ehewet1 <ehewett32425@live.com>
2021-09-27 01:49:56 -0500

    3rd Commit - End of Ch 23/Mod 6 assignment

--- app/src/main/java/com/raywenderlich/podplay/viewmodel/PodcastViewModel.kt
+++ app/src/main/java/com/raywenderlich/podplay/viewmodel/PodcastViewModel.kt
@@ -2,75 +2,54 @@ package com.raywenderlich.podplay.viewmodel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
 import com.raywenderlich.podplay.model.Episode
 import com.raywenderlich.podplay.model.Podcast
 import com.raywenderlich.podplay.repository.PodcastRepo
+import kotlinx.coroutines.launch
 import java.util.*
 
 class PodcastViewModel (application: Application):
 AndroidViewModel(application){
 
     var podcastRepo: PodcastRepo? = null
-    var activePodcastViewData: PodcastViewData? = null
+    private val _podcastLiveData = MutableLiveData<PodcastViewData?>()
+    val podcastLiveData: LiveData<PodcastViewData?> = _podcastLiveData
 
-    data class PodcastViewData(
-        var subscribed: Boolean = false,
-        var feedTitle: String? = "",
-        var feedUrl: String? = "",
-        var feedDesc: String? = "",
-        var imageUrl: String? = "",
-        var episode: List<EpisodeViewData>
-    )
-
-    data class EpisodeViewData(
-        var guid: String? = "",
-        var title: String? = "",
-        var description: String? = "",
-        var mediaUrl: String? = "",
-        var releaseDate: Date?= null,
-        var duration: String? = ""
-    )
-
-    private fun episodesToEpisodeView(episodes: List<Episode>):
-    List<EpisodeViewData>{
-        return episodes.map{
-            EpisodeViewData(
-                it.guid,
-                it.title,
-                it.description,
-                it.mediaUrl,
-                it.releaseDate,
-                it.duration
-            )
+    fun getPodcast(podcastSummaryViewData: SearchViewModel.PodcastSummaryViewData) {
+        podcastSummaryViewData.feedUrl?.let { url ->
+            viewModelScope.launch {
+                podcastRepo?.getPodcast(url)?.let {
+                    it.feedTitle = podcastSummaryViewData.name ?: ""
+                    it.imageUrl = podcastSummaryViewData.imageUrl ?: ""
+                    _podcastLiveData.value = podcastToPodcastView(it)
+                } ?: run {
+                    _podcastLiveData.value = null
+                }
+            }
+        } ?: run {
+            _podcastLiveData.value = null
         }
     }
 
-    private fun podcastToPodcastView(podcast: Podcast):PodcastViewData{
-        return PodcastViewData(
-            false,
-            podcast.feedTitle,
-            podcast.feedUrl,
-            podcast.feedDesc,
-            podcast.imageUrl,
-            episodesToEpisodeView(podcast.episode)
-        )
+    private fun podcastToPodcastView(podcast: Podcast): PodcastViewData {
+        return PodcastViewData(false, podcast.feedTitle, podcast.feedUrl, podcast.feedDesc,
+            podcast.imageUrl, episodesToEpisodesView(podcast.episode))
     }
 
-    fun getPodcast(podcastSummaryViewData: SearchViewModel.PodcastSummaryViewData): PodcastViewData?{
-        val repo = podcastRepo ?: return null
-        val feedUrl = podcastSummaryViewData.feedUrl ?: return null
-
-        val podcast = repo.getPodcast(feedUrl)
-
-        podcast?.let{
-
-            it.feedTitle = podcastSummaryViewData.name ?: ""
-            it.imageUrl = podcastSummaryViewData.imageUrl ?: ""
-            activePodcastViewData = podcastToPodcastView(it)
-
-            return activePodcastViewData
+    private fun episodesToEpisodesView(episodes: List<Episode>): List<EpisodeViewData> {
+        return episodes.map {
+            EpisodeViewData(it.guid, it.title, it.description, it.mediaUrl, it.releaseDate, it.duration)
         }
-
-        return null
     }
+
+    data class PodcastViewData(var subscribed: Boolean = false, var feedTitle: String? = "",
+                               var feedUrl: String? = "", var feedDesc: String? = "",
+                               var imageUrl: String? = "", var episodes: List<EpisodeViewData>)
+
+    data class EpisodeViewData(var guid: String? = "", var title: String? = "",
+                               var description: String? = "", var mediaUrl: String? = "",
+                               var releaseDate: Date? = null, var duration: String? = "")
 }
